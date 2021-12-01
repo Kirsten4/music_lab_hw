f1a0462bc0af6261501a987a0050710d64c3de09
Kirsten Buchan <kirsten2412@hotmail.co.uk>
2021-12-01 09:37:22 +0000

    corrections

--- console.py
+++ console.py
@@ -22,23 +22,19 @@ album_repository.save(album_2)
 album_3 = Album("Only Revolutions", "Rock", artist_1)
 album_repository.save(album_3)
 
-artist_2_id = artist_2.id
-print(artist_repository.select(artist_2_id).name)
+print(artist_repository.select(artist_2.id).name)
 
-album_3_id = album_3.id
-print(album_repository.select(album_3_id).title)
+print(album_repository.select(album_3.id).title)
 
-album_3 = Album("Only Revolutions", "Pop", artist_1)
+album_3.genre = "Pop"
 album_repository.update(album_3)
 
-artist_1 = Artist("Biffy Biffy Clyro")
+artist_1.name = "Clyro Biffy" 
 artist_repository.update(artist_1)
 
-album_2_id = album_2.id
-album_repository.delete(album_2_id)
+album_repository.delete(album_2.id)
 
-artist_2_id = artist_2.id
-artist_repository.delete(artist_2_id)
+artist_repository.delete(artist_2.id)
 
 artists = artist_repository.select_all()
 albums = album_repository.select_all()
